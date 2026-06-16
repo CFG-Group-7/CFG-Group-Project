b1472f76aa4fbfdf95533e473dc8b93394af94ec
hannahsh45 <han.sharpe@hotmail.co.uk>
2026-06-16 21:13:11 +0100

    Pushing javascript for 5 quiz questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
+import Search from './pages/Search.jsx';
 
 function App() {
 
   return (
     <>
       <h1>Welcome to Zoopedia</h1>
+      <Search />
     </>
   );
 }
-
-export default App;
