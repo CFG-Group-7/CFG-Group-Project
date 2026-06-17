cb00dc18bba55843d2755ab4419f036181f50746
Keplin1 <188714815+Keplin1@users.noreply.github.com>
2026-06-17 01:08:35 +0100

    updated the main.jsx component, set up routes for the search and flashcards components. Added animal context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Search from './pages/Search.jsx';
+import FlashCards from './pages/FlashCards.jsx';
 import './index.css';
 import App from './App.jsx';
+import { Routes, Route } from "react-router-dom"
+import { AnimalProvider } from './AnimalContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <AnimalProvider>
+
+        {/* nav bar goes here */}
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/flash-cards' element={<FlashCards />} />
+        </Routes>
+      </AnimalProvider>
+    </StrictMode>
+  </BrowserRouter >
 );
---
+++ src/pages/FlashCards.jsx
@@ -0,0 +1,11 @@
+export default function FlashCards() {
+    return (
+
+
+        <>
+            <h1>FLASH CARDS COMPONENT</h1>
+        </>
+    )
+
+
+}
