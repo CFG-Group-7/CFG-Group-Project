fa2396f6ecd419dd424af2cfa30596a56aec1666
Beth Hobbs <bethanie.hobbs@gmail.com>
2026-06-17 16:49:42 +0100

    suggested updates to App.jsx & main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import FlashCards from './pages/FlashCards';
+import Quiz from './pages/Quiz';
+import MoreFun from './pages/MoreFun';
+
 
 function App() {
 
   return (
     <>
       <h1>Welcome to Zoopedia</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/flashcards' element={<FlashCards />} />
+        <Route path='/quiz' element={<Quiz />} />
+        <Route path='/more' element={<MoreFun />} />
+      </Routes>
     </>
   );
 }
