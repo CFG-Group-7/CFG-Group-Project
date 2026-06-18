c16f24ec7441977ac7a8b23d2b5f44a1a6c9651f
hannahsh45 <han.sharpe@hotmail.co.uk>
2026-06-18 16:53:13 +0100

    adding more questions to the question bank

--- src/components/Quiz/questionBank.js
+++ src/components/Quiz/questionBank.js
@@ -31,6 +31,48 @@ const qBank = [
     answer: "Pouch",
     id: 5
     },
+    {
+    question: "What is a group of fish called?",
+    options: ["Club", "School", "College", "Home"],
+    answer: "School",
+    id: 6
+    },
+       {
+    question: "How many teeth does a shark have?",
+    options: ["3", "30", "300", "3000"],
+    answer: "3000",
+    id: 7
+    },
+       {
+    question: "Which animal can rotate its head three quarters of the way around?",
+    options: ["Lion", "Hippo", "Owl", "Lizard"],
+    answer: "Owl",
+    id: 8
+    },
+       {
+    question: "How many legs does an octopus have?",
+    options: ["2", "4", "6", "8"],
+    answer: "8",
+    id: 9
+    },
+       {
+    question: "Which is the fastest cat?",
+    options: ["Tiger", "Cheetah", "Lion", "Leopard"],
+    answer: "Cheetah",
+    id: 10
+    },
+       {
+    question: "Which animal loves to swing in trees and eat bananas?",
+    options: ["Monkey", "Meerkat", "Sloth", "Badger"],
+    answer: "Monkey",
+    id: 11
+    },
+       {
+    question: "What is the smelliest animal?",
+    options: ["Goat", "Hippo", "Snake", "Skunk"],
+    answer: "Skunk",
+    id: 12
+    },
     ];
 
 export default qBank;
