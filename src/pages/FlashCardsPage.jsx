5058de0b5309635e854a575b3d96cb727a234840
Keplin1 <188714815+Keplin1@users.noreply.github.com>
2026-06-18 23:44:48 +0100

    cleaned up the flash cards page contexts

--- src/pages/FlashCardsPage.jsx
+++ src/pages/FlashCardsPage.jsx
@@ -2,15 +2,13 @@ import { useEffect, useState } from "react";
 import { useAnimals, fetchAnimal } from "../AnimalContext";
 import FlashCard from "../components/FlashCard";
 
-
-
-
-
 export default function FlashCardsPage() {
-    const { animals, setAnimals, setAnimalData, setSearchData, searchData, cacheSearchData } = useAnimals(); // array of default animal names from the context 
+    // array of default animal names from the context 
+    const { animals, searchData, cacheSearchData } = useAnimals();
     const [currentIndex, setCurrentIndex] = useState(0);
     const currentName = animals[currentIndex]; // animals[0] - 'camel'
-    // we are checking if Rachel has camels, if she doesnt then we have to go and find them ourselves 
+
+    // here, we are checking if Rachel has camels, if she doesnt then we have to go and find them ourselves 
     const animalData = searchData.find((animal) => animal.animalName.toLowerCase() === currentName.toLowerCase()); // looks at each animal name from the search results and checks if it exists in the list of animal names from the default list 
 
 
