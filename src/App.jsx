2cd76d0184067a9492366fcbb1507c49b1b5d956
Keplin1 <188714815+Keplin1@users.noreply.github.com>
2026-06-20 22:53:53 +0100

    fixed imports in quiz and updated routes in main.jsx by adding the quiz to them

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,34 @@ import { useState } from 'react';
 import './App.css';
 import FeatureCard from "./components/FeatureCard";
 
-function App() {
+export default function App() {
 
-  return (
+    return (
 
-    <>
-      <h1>Welcome to Zoopedia</h1>
-         <FeatureCard
-             title="Find an animal"
-             description=" Search for your favourite zoo animal and learn all about it!"
-             pageLink="/search" />
-     
-         <FeatureCard
-             title="Flash Cards"
-             description=" Flip cards to learn fun facts about 15 different zoo animals!"
-             pageLink="/flash-cards-page" />
-     
-         <FeatureCard
-             title="Take a quiz"
-             description=" How much do you know about zoo animals?"
-             pageLink="/quiz"
-         />
-     
-         <FeatureCard
-             title="More Fun"
-             description=" Search for your favourite zoo animal and learn all about it!"
-             pageLink="/more-fun"
-         />
-    </>
-  );
+        <>
+            <h1>Welcome to Zoopedia</h1>
+            <FeatureCard
+                title="Find an animal"
+                description=" Search for your favourite zoo animal and learn all about it!"
+                pageLink="/search" />
+
+            <FeatureCard
+                title="Flash Cards"
+                description=" Flip cards to learn fun facts about 15 different zoo animals!"
+                pageLink="/flash-cards" />
+
+            <FeatureCard
+                title="Take a quiz"
+                description=" How much do you know about zoo animals?"
+                pageLink="/quiz"
+            />
+
+            <FeatureCard
+                title="More Fun"
+                description=" Search for your favourite zoo animal and learn all about it!"
+                pageLink="/more-fun"
+            />
+        </>
+    );
 }
+
