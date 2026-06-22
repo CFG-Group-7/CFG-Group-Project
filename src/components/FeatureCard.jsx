8d75cd95443e6a9edb501ceadf71a760bd55cd74
sims-bit <kathrynsims1996@gmail.com>
2026-06-22 13:41:09 +0100

    Add universal styling to featureCard component. Set text size. Set pagelink background colour and font colour. Set border radius for pagelink.

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -7,9 +7,9 @@ import { Link } from "react-router-dom";
 export default function FeatureCard(props) {
     return (
         <div>
-            <h2>{props.title}</h2>
-            <p>{props.description}</p>
-            <Link to={props.pageLink}>Lets go</Link>
+            <h2 className="text-xl">{props.title}</h2>
+            <p className="mb-2">{props.description}</p>
+            <Link className="bg-[#3B5640] text-[#FEFAE0] text-base rounded-xl w-96 p-1 pr-8 pl-8" to={props.pageLink}>Lets go</Link>
         </div>
     );
 }
