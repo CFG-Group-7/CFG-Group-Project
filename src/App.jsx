ceca7d2667f2fc6b7f94d252ccf0221a042e1f3e
sims-bit <kathrynsims1996@gmail.com>
2026-06-22 13:45:08 +0100

    Added individual colours and  styling to each feature card

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,49 @@ import FeatureCard from "./components/FeatureCard";
 
 function App() {
 
-  return (
+    return (
 
-    <>
-      <h1>Welcome to Zoopedia</h1>
-         <FeatureCard
-             title="Find an animal"
-             description=" Search for your favourite zoo animal and learn all about it!"
-             pageLink="/search" />
-     
-         <FeatureCard
-             title="Flash Cards"
-             description=" Flip cards to learn fun facts about 15 different zoo animals!"
-             pageLink="/flash-cards-page" />
-     
-         <FeatureCard
-             title="Take a quiz"
-             description=" How much do you know about zoo animals?"
-             pageLink="/quiz"
-         />
-     
-         <FeatureCard
-             title="More Fun"
-             description=" Search for your favourite zoo animal and learn all about it!"
-             pageLink="/more-fun"
-         />
-    </>
-  );
+        <>
+
+            <div className="text-center">
+                <h1>Welcome to Zoopedia</h1>
+            </div>
+            <div className="flex ml-6 mr-6">
+                <div className="bg-[#E55934] rounded-xl text-center m-2 p-6">
+                    <FeatureCard
+                        title="Find an animal"
+                        description=" Search for your favourite zoo animal and learn all about it!"
+                        pageLink="/search"
+                    />
+                </div>
+
+                <div className="bg-[#5BC0EB] rounded-xl text-center m-2 p-6">
+                    <FeatureCard
+                        title="Flash Cards"
+                        description=" Flip cards to learn fun facts about 15 different zoo animals!"
+                        pageLink="/flash-cards-page"
+                    />
+                </div>
+
+                <div className="bg-[#9BC53D] rounded-xl text-center m-2 p-6">
+                    <FeatureCard
+                        title="Take a quiz"
+                        description=" How much do you know about zoo animals?"
+                        pageLink="/quiz"
+                    />
+                </div>
+
+                <div className="bg-[#FDE74C] rounded-xl text-center m-2 p-6">
+                    <FeatureCard
+                        title="More Fun"
+                        description=" Search for your favourite zoo animal and learn all about it!"
+                        pageLink="/more-fun"
+                    />
+                </div>
+            </div>
+
+        </>
+    );
 }
 
 export default App;
