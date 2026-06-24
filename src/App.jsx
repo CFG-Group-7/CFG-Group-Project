c48af1ad60e4e5e1230cd852d11cd2df1018582b
bmh333 <bethanie.hobbs@gmail.com>
2026-06-24 13:49:09 +0100

    Remove FeatureCard components from App.jsx
    Removed multiple FeatureCard components from the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,6 @@ export default function App() {
     return (
 
         <>
-            <h1>Welcome to Zoopedia</h1>
-            <FeatureCard
-                title="Find an animal"
-                description=" Search for your favourite zoo animal and learn all about it!"
-                pageLink="/search" />
-
-            <FeatureCard
-                title="Flash Cards"
-                description=" Flip cards to learn fun facts about 15 different zoo animals!"
-                pageLink="/flash-cards" />
-
-            <FeatureCard
-                title="Take a quiz"
-                description=" How much do you know about zoo animals?"
-                pageLink="/quiz"
-            />
-
-function App() {
-
-    return (
-
-        <>
 
             <div className="text-center">
                 <h1>Welcome to Zoopedia</h1>
