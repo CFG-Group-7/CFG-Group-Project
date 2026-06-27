01152454806f995951f20fbf3be3c51387136fe5
sims-bit <kathrynsims1996@gmail.com>
2026-06-27 12:43:03 +0100

    Fixed text wrapping on link display. Updated colour to use css variables and updated font.

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -6,7 +6,7 @@ export default function FeatureCard(props) {
         <div>
             <h2 className="text-xl">{props.title}</h2>
             <p className="mb-2">{props.description}</p>
-            <Link className="bg-dark-green text-[#FEFAE0] font-[irish-grover] text-base rounded-xl p-1 px-8" to={props.pageLink}>Lets go</Link>
+            <Link className="bg-dark-green text-pale-yellow font-logo text-base text-nowrap rounded-xl p-1 px-8" to={props.pageLink}>Lets go</Link>
         </div>
     );
 }
