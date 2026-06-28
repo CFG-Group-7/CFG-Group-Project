ecd1363417a1c40a6002321ba5f0c57373f97978
Keplin1 <188714815+Keplin1@users.noreply.github.com>
2026-06-28 16:06:07 +0100

    updated funfact test file

--- src/tests/FunFact.spec.jsx
+++ src/tests/FunFact.spec.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import { cleanup, render, screen } from "@testing-library/react";
-import { describe, it, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import FunFact from "../components/FunFact";
 
 describe("FunFact.jsx test", () => {
 
 
-    test("renders fun fact heading", () =>{
+    test("renders fun fact heading", () => {
         render(<FunFact />);
-        
+
         expect(screen.getByText("Fun Fact!")).toBeInTheDocument();
     })
 
